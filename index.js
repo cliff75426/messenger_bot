e5727ee8d0022624649c21b1750c6e498d402b4c
cliff75426 <cliffchen75426@gmail.com>
2018-06-04 21:49:46 +0800

    fix

--- index.js
+++ index.js
@@ -32,14 +32,18 @@ const token = "EAAJL7l1CkEsBAPVosMfliFQqZCHB7N1PaZALO6dIAaZCzXDHT559PVddCzZCMHYp
 
 app.post('/webhook/', function(req, res) {
     var messaging_events = req.body.entry[0].messaging;
-    for (var i = 0; i < messaging_events.length; i++) {
-        var event = req.body.entry[0].messaging[i];
-        var sender = event.sender.id;
-        if (event.message && event.message.text) {
-            var text = event.message.text;
-            sendTextMessage(sender, text + "!");
-        }
-    }
+  //  for (var i = 0; i < messaging_events.length; i++) {
+  //      var event = req.body.entry[0].messaging[i];
+  //      var sender = event.sender.id;
+  //      if (event.message && event.message.text) {
+  //          var text = event.message.text;
+  //          sendTextMessage(sender, text + "!");
+  //      }
+  //  }
+    var event = req.body.entry[0].messaging[0];
+    var sender = event.sender.id;
+    var text = event.message.text;
+    senTextMessage(sender, text+!);
     res.sendStatus(200);
 });
 function sendTextMessage(sender, text) {
