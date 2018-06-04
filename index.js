42454a57e7811e0236d5aa07ac1c18ba5fbd5bbd
cliff75426 <cliffchen75426@gmail.com>
2018-06-05 00:21:55 +0800

    fix

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post('/webhook/', function(req, res) {
     console.log(counter);
     console.log(req.body.entry[0]);
   console.log(req.body.entry[0].standby);
-  console.log('@@testtag'+req.body.entry[0].messaging);
+  console.log('@@testtag'+req.body.entry[0].messaging[0]);
   console.log('@@testtag'+req.body.entry[0].messaging.recipient);
   console.log(req.body.entry[0].standby.message);
   console.log(req.body.entry[0].standby.recipient);
