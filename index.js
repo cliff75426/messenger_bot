d863ea0528ec3a1be237205f0e1643db7f0f54fe
cliff75426 <cliffchen75426@gmail.com>
2018-06-16 02:17:24 +0800

    parameter   123

--- index.js
+++ index.js
@@ -41,7 +41,6 @@ app.post('/webhook/', function(req, res) {
   if(data && data.object === 'page'){
     for( var i = 0; i < data.entry[0].standby.length; i++){
       var event = data.entry[0].standby[i];
-      console.log(event);
       var senderID = event.sender.id;
       if(event.message){
         handleMessage(senderID, event);
@@ -62,6 +61,8 @@ function handleMessage(senderID,event){
     var request = dialogflow.textRequest(received_message.text, {sessionId: senderID});
     request.on('response', function(response){
       console.log("訊息處理");
+      console.log('REQUEST: '+ JSON.stringify(request));
+      console.log('Response: '+ JSON.stringify(response));
       if(response.result.action == "train"){
         strStation = response.result.parameters.start_station;
         arrStation = response.result.parameters.arrive_station;
