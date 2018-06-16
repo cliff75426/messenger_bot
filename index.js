65f1c8136024558943d0e0e663fd0b7f69c54f7b
cliff75426 <cliffchen75426@gmail.com>
2018-06-16 15:19:50 +0800

    parameter   123

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const apiai = require('apiai');
-const cheerio = require('cheerio');
-const moment = require('moment');
-const moment_timezone = require('moment-timezone');
+var cheerio = require('cheerio');
+var moment = require('moment');
+var moment_timezone = require('moment-timezone');
 
 
 const dialogflow = apiai("6522ed4d07fe42f09edd4963b477b2ca","2bec7b4562fa4c6e81db5188e5c54713");
