adb39c603dfc3382683bb0d337bf9585d74e071d
cliff75426 <cliffchen75426@gmail.com>
2018-06-24 00:39:13 +0800

    elasticsearch-1

--- index.js
+++ index.js
@@ -226,6 +226,7 @@ function elasticsearch_result( from_number, size_number, query_string){
     }
     }).then(function (resp) {
       var hits = resp.hits.hits;
+      console.log(hits);
     }, function (err) {
       console.trace(err.message);
   });
